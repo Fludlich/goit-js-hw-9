66058b11d9f0e186ce8593b78c994dd5f822040d
Yurii Vynar <yurik.vinar37@gmail.com>
2022-10-20 00:23:55 +0300

    promise

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,22 +1,22 @@
 import flatpickr from "flatpickr";
-console.log(flatpickr)
+
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
-console.log(Notiflix)
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 Notiflix.Notify.init({
-  clickToClose: true,
   showOnlyTheLastOne: true,
-  closeButton: true,
-
+  timeout: 1,
 })
+// 
 const falsee=()=>{
-  Notiflix.Notify.failure(
-    'Choose the correct date',
-    function cb() {
-      // callback
+  Notify.failure('Qui timide rogat docet negare');
+  // Notiflix.Notify.failure(
+  //   'Choose the correct date',
+  //   function cb() {
+  //     // callback
       
-    },
-  );
+  //   },
+  // );
 }
 const falseeDate=()=>{
   Notiflix.Notify.failure(
@@ -42,17 +42,17 @@ const truee=()=>{
 
 const chooseDate = document.getElementById('datetime-picker')
 const startBtn = document.querySelector('[data-start]')
-const timerHtml = document.querySelectorAll('.value')
-console.log(timerHtml)
-
-
-// for (const text of myValue.textContent) {
-//   console.log(text)
-// }
-
-
-console.log(startBtn)
+let timerDays = document.querySelector('[data-days]')
+let timerHours = document.querySelector('[data-hours]')
+let timerMinutes = document.querySelector('[data-minutes]')
+let timerSeconds = document.querySelector('[data-seconds]')
+let arar
+let result 
 let stopTimer = 0 
+
+
+
+
 startBtn.addEventListener('click', (event)=>{
   stopTimer+=1
   if (stopTimer === 1){
@@ -60,13 +60,7 @@ startBtn.addEventListener('click', (event)=>{
   }
  
 })
-let timerDays = document.querySelector('[data-days]')
 
-let timerHours = document.querySelector('[data-hours]')
-
-let timerMinutes = document.querySelector('[data-minutes]')
-
-let timerSeconds = document.querySelector('[data-seconds]')
 
 const options = {
   enableTime: true,
@@ -76,15 +70,14 @@ const options = {
   onChange (selectedDates){
     const dateValid= selectedDates[0]-new Date()
     if (dateValid<0){
-          // console.log('choose valid date')
-          // console.log(dateValid)
           falsee()
           localStorage.setItem('userDate', 1000)
-          
+          timerDays.textContent = '00'
+          timerHours.textContent = '00'
+          timerMinutes.textContent= '00'
+          timerSeconds.textContent= '00'
         }
         if(dateValid>0){
-              console.log('youre date valid')
-
           truee()
             }
   },
@@ -95,33 +88,36 @@ const options = {
       Notiflix.Notify.failure(
         'Choose the correct date',
         function cb() {
-          // callback
         },
       );
       return
     }
     localStorage.setItem('userDate', Date.parse(selectedDates[0]))
-    // console.log(selectedDates[0]);
+    const currrentTime = Date.now()
   
+    arar =currrentTime - selectedDates[0]
+    convertMs(+-arar)
+
+    timerDays.textContent = result.days
+    timerHours.textContent = result.hours
+    timerMinutes.textContent= result.minutes
+    timerSeconds.textContent= result.seconds
+
+
   },
   
 };
 
 flatpickr(chooseDate, options)
 
-// const userDate = localStorage.getItem('userDate')- Date.now()
 
-// console.log(userDate)
-
-
-
-let result 
 
 
 
 function pad(value){
   return String(value).padStart(2, '0')
 }
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -140,27 +136,7 @@ function convertMs(ms) {
 
   return result={days, hours, minutes, seconds};
 }
-convertMs()
 
-// console.log(result.map(num=>num.textContent))
-
-
-// function setTimerTime({ days, hours, minutes, seconds }){
-
-//   console.log(myValue.map(num => {num.textContent=`${days}`}))
-// }
-// setTimerTime()
-// for (const val of result) {
-//   console.log(val)
-// }
-// const ararrarrr=[]
-// console.log(result)
-// for (const value in result) {
-// console.log(value)
-// ararrarrr.push(value)
-    
-//   }
-//   console.log(ararrarrr)
 
 
 const timer = {
@@ -169,53 +145,42 @@ const timer = {
     const startTime = localStorage.getItem('userDate') 
     const currentTime= Date.now()
     if(startTime<currentTime){
-      console.log('4')
-      // clearInterval(tiimerId)
       stopTimer=0
       startBtn.textContent = 'Start'
       falseeDate()
       return
      }
     const tiimerId = setInterval(() =>{
-      const currrentTime= Date.now()
+    const currrentTime= Date.now()
       
-      const arar =currrentTime - startTime
+    arar =currrentTime - startTime
    
-    console.log(convertMs(+-arar))
-        timerDays.textContent = result.days
-        timerHours.textContent = result.hours
-        timerMinutes.textContent= result.minutes
-        timerSeconds.textContent= result.seconds
-        console.log(arar)
-    // for (let index = 0; index < timerHtml.length; index++) {
-    //   const element = timerHtml[index];
-    //   console.log(element.textContent=result.map(num=>num))
-      
-      
-    // }
-   
+    convertMs(+-arar)
+
+    timerDays.textContent = result.days
+    timerHours.textContent = result.hours
+    timerMinutes.textContent= result.minutes
+    timerSeconds.textContent= result.seconds
+
+
 
     if(arar>-1000){
-      console.log('1')
-      console.log(arar)
       clearInterval(tiimerId)
       stopTimer=0
       startBtn.textContent = 'Start'
     }
     if(stopTimer>1){
       if(arar<-1000){
-        console.log('2')
         startBtn.textContent = 'Resume'
         clearInterval(tiimerId)
         stopTimer=0
         return
       }
-      console.log('3')
       clearInterval(tiimerId)
       stopTimer=0
       startBtn.textContent = 'Start'
     }
-  // console.log(result)
+
 
     }, 1000)
    
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,53 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form')
+const btn = document.querySelector('button')
+
+
+let storage ={}
+const inputAmount = storage.amount
+
+btn.addEventListener('click', (event)=>{
+ event.preventDefault()
+
+ quantity()
+
+
+})
+
+form.addEventListener('input', (event)=>{
+  storage[event.target.name]=event.target.value
+  console.log(storage)
+
+
+})
+
+function quantity (){
+console.log(storage.amount)
+console.log(storage.delay)
+  for (let index = 1; index <= storage.amount; index++) {
+
+    createPromise(index, storage.step)
+  }
+}
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+ setTimeout(()=>{
   if (shouldResolve) {
-    // Fulfill
+   
+     Notify.success(`Rejected promise ${position} in ${Number(position)*Number(delay)}ms`);
+   
+  
+
   } else {
-    // Reject
+  
+   
+     Notify.failure(`Rejected promise ${position} in ${Number(position)*Number(delay)}ms`);
+    
+
   }
+ }, (Number(storage.delay)+Number(delay)*Number(position)))
 }
